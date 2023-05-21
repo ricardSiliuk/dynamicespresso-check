e5eef2c9deb4b372c63796fa23a410b6fb868394
ricardSiliuk <ricard.siliuk@gmail.com>
2023-05-21 13:39:59 +0300

    Init commit

---
+++ ProtosAndExtensions/DateExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Overwriting namespace to add extensions to correct class
+// ReSharper disable once CheckNamespace
+
+namespace Extendable
+{
+    public sealed partial class Date
+    {
+        // example of conversion func
+        private static Date StringToDate(string dateRepr)
+        {
+            var parts = dateRepr.Split("-");
+            return new Date
+            {
+                Year = int.Parse(parts[0]),
+                Month = int.Parse(parts[1]),
+                Day = int.Parse(parts[2])
+            };
+        }
+
+        // Since we can only have one == overload, we'd need to define bunch of these for every conceivable comparison.
+        // Shouldn't be too many tho.
+        public static implicit operator Date(string dateRepr) => StringToDate(dateRepr);
+
+        public static bool operator ==(Date? left, Date? right)
+        {
+            // not sure how I'd avoid NPE here, as I can't check if it's equal to null :D
+            return left.Year == right.Year && left.Month == right.Month && left.Day == right.Day;
+        }
+
+        public static bool operator !=(Date? left, Date? right)
+        {
+            return !(left == right);
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DynamicExpresso;
+using Extendable;
+
+var helloRequests = new List<HelloRequest>
+{
+    new()
+    {
+        Name = "me",
+        Date = new Date { Day = 1, Month = 2, Year = 1970 }
+    },
+    new()
+    {
+        Name = "you",
+        Date = new Date { Day = 1, Month = 1, Year = 1970 }
+    }
+};
+
+var equalDate = "1970-02-01";
+
+// Verify equality comparison works
+Console.WriteLine(helloRequests[0].Date == equalDate);
+Console.WriteLine(helloRequests[1].Date == equalDate);
+
+// Verify equality comparison works in LINQ
+Console.WriteLine(helloRequests.Where(request => request.Date == equalDate).ToList());
+
+// Try the same with expresso
+var interpreter = new Interpreter().Reference(typeof(Extendable.Date));
+var whereFunction = interpreter.ParseAsDelegate<Func<HelloRequest, bool>>($"""request.Date == "{equalDate}" """, "request");
+
+Console.WriteLine(helloRequests.Where(whereFunction).ToList());
+
+Console.ReadLine();
